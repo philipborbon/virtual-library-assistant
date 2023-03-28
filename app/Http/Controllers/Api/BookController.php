7743119852b0e0d0fa5eafa1ab1eb97c51851d4e
Philip Borbon <philipjohnborbon@gmail.com>
2023-03-29 02:04:33 +0800

    Getting book list, posting borrow request

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -4,16 +4,33 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Models\Book;
+use App\Models\History;
 
 class BookController extends Controller
 {
     public function getBooks($categoryId, Request $request)
     {
-        // TODO:
+        return Book::where('category_id', $categoryId)->get();
     }
 
     public function requestBorrow($bookId, Request $request)
     {
-        // TODO:
+        $book = Book::find($bookId);
+
+        if (! $book) {
+            abort(404);
+        }
+
+        $user = $request->user();
+
+        History::create([
+            'user_id' => $user->id,
+            'book_id' => $book->id,
+        ]);
+
+        return response()->noContent(Response::HTTP_CREATED);
     }
 }
