f62fa5d09b25e5b3dd52ebdd5749e583316fae61
Philip Borbon <philipjohnborbon@gmail.com>
2023-04-10 05:15:14 +0800

    Update CategoryResource.php

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -17,6 +17,7 @@ class CategoryResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'path' => $this->full_path,
             'books' => $this->books_count,
             'categories' => $this->categories_count
         ];
